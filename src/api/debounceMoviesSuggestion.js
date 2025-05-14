2885ff28ae776e42677aff1ff2437a99b0996995
rodrilucas <lucas.celery@gmail.com>
2025-05-14 15:32:21 -0300

    feat: search request for movie suggestions using keywords

---
+++ src/api/debounceMoviesSuggestion.js
@@ -0,0 +1,37 @@
+import { delay } from "../utils/delay";
+import { debounceMoviesSuggestionState } from "../states/debounceMoviesSuggestionState";
+
+async function debounceMoviesSuggestion(query, limit, fetchDelay) {
+  try {
+    const response = await Promise.all([
+      fetch(
+        `http://localhost:3000/api/v1/movies/suggestions?keyword=${encodeURIComponent(
+          query
+        )}&limit=${limit}`,
+        {
+          headers: {
+            "x-api-key": import.meta.env.VITE_API_KEY,
+          },
+        }
+      ),
+      delay(fetchDelay),
+    ]).then(([res]) => res);
+
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(data.message || "Sem informações sobre sugestões de filmes");
+
+    if (!data.movies || data.movies.length === 0) {
+      debounceMoviesSuggestionState.debounceMoviesSuggestionError = "Nenhuma palavra-chave encontrada.";
+      return [];
+    }
+
+    return data.movies;
+  } catch (error) {
+    debounceMoviesSuggestionState.debounceMoviesSuggestionError =
+      error.message || "Erro ao buscar palavras-chave de filmes.";
+    return [];
+  }
+}
+
+export { debounceMoviesSuggestion };
