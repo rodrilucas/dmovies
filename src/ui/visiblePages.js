f7540df4bc05c8351a2f445d48499d2b1291fad8
rodrilucas <lucas.celery@gmail.com>
2025-05-16 16:48:37 -0300

    feat: logic for page visibility

---
+++ src/ui/visiblePages.js
@@ -0,0 +1,28 @@
+export function visiblePages(current, total) {
+  const delta = 2;
+  const range = [];
+
+  range.push(1);
+
+  if (current - delta > 2) {
+    range.push("...");
+  }
+
+  for (
+    let i = Math.max(2, current - delta);
+    i <= Math.min(total - 1, current + delta);
+    i++
+  ) {
+    range.push(i);
+  }
+
+  if (current + delta < total - 1) {
+    range.push("...");
+  }
+
+  if (total > 1) {
+    range.push(total);
+  }
+
+  return range;
+}
