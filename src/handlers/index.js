e97423a1668940f83ea7479070addf927fcec258
rodrilucas <lucas.celery@gmail.com>
2025-06-07 20:00:23 -0300

    refactor: centralize the import of classes

---
+++ src/handlers/index.js
@@ -0,0 +1,6 @@
+export { Loading } from "./classes/Loading";
+export { Filter } from "./classes/Filter";
+export { Genre } from "./classes/Genre";
+export { Keyword } from "./classes/Keyword";
+export { Modal } from "./classes/Modal";
+export { Search } from "./classes/Search";
